104e4169715d480adf878d9158354febca1f4705
ElUltimoPasajero <elultimopasajero87@gmail.com>
2024-03-11 14:47:57 +0100

    Cosa

--- src/main/java/com/example/examen/HelloApplication.java
+++ src/main/java/com/example/examen/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 500
         );
-        stage.setTitle("Hello!");
+        stage.setTitle("Examen Interfaces!");
         stage.setScene(scene);
         stage.show();
     }
